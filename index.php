a97c453c787c32fd7fb05f9346fa164c2e81d502
Elly Ochieng <ellyaston404@gmail.com>
2025-03-02 12:55:20 +0300

    assignment1 IAP

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Website</title>
+</head>
+<body>
+	<a href="logout.php">Logout</a>
+	<h1>This is the index page</h1>
+
+	<br>
+	Hello, Username.
+</body>
+</html>
